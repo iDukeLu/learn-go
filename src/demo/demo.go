191d265b60f98a8f9905c0313cb4a5107717aa07
duke.lu <duke.lu@ideamake.cn>
2020-06-12 21:20:37 +0800

    init

---
+++ src/demo/demo.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+}
+
+// 变量的三种声明方式
+func varDemo() {
+	var i int
+	var j = 1
+	k := 3
+	fmt.Printf("无赋值声明：%v", i)
+	fmt.Printf("自动类型检测：%v", j)
+	fmt.Printf("短声明，仅能在函数中使用：%v", k)
+}
+
+//
