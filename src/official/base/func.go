967d75094735f52c0d2d37906c3f6562c7662609
duke.lu <duke.lu@ideamake.cn>
2020-12-07 19:06:35 +0800

    Add Some Demo

--- src/official/base/func.go
+++ src/official/base/func.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+const (
+	// 将 1 左移 100 位来创建一个非常大的数字
+	// 即这个数的二进制是 1 后面跟着 100 个 0
+	Big = 1 << 100
+	// 再往右移 99 位，即 Small = 1 << 1，或者说 Small = 2
+	Small = Big >> 99
+)
+
+var ()
+
 func main() {
 	fmt.Println(add1(42, 13))
 }
---
+++ src/official/base/var.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+var (
+	s string
+	i = 1
+)
+
+func main() {
+	var f float64
+	var f1 float64 = 1
+	var f2 = 1
+	f3 := 1
+
+	fmt.Print(i)
+	fmt.Print(s)
+	fmt.Print(f)
+	fmt.Print(f1)
+	fmt.Print(f2)
+	fmt.Print(f3)
+}
