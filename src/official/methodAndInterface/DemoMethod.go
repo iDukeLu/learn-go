789c72c27c57a19f84b22be9019f40b56180c330
duke.lu <duke.lu@ideamake.cn>
2020-12-16 18:27:41 +0800

    add some demo

--- src/official/methodAndInterface/DemoMethod.go
+++ src/official/methodAndInterface/DemoMethod.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	methodDemo()
+	methodDemo1()
 }
 
 // Go 没有类。不过你可以为结构体类型定义方法。
 // 方法就是一类带特殊的 接收者 参数的函数。
 // 方法接收者在它自己的参数列表内，位于 func 关键字和方法名之间。
-func methodDemo() {
+func methodDemo1() {
 	user := new(User)
 	user.setName("张三")
 	fmt.Println(user)
